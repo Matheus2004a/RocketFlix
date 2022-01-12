cb05e823d5b97c0a1749829532aef3b8f2bcdc2d
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-12 14:00:02 -0300

    Adicionando template

---
+++ api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'api_key=1cbe711fae00a46e51acc682f422c268';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=1cbe711fae00a46e51acc682f422c268';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ main.js
@@ -0,0 +1,9 @@
+import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
+
+const buttonSearchMovie = document.querySelector(".search-movie")
+buttonSearchMovie.addEventListener("click", searchMovie)
+
+async function searchMovie() {
+  const url = BASE_URL
+  
+}
