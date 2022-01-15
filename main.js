7842a39d454a3fcbebde3c31e759c4663b294557
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-15 13:35:07 -0300

    Realizando requisição a api TMDB

--- main.js
+++ main.js
@@ -5,5 +5,22 @@ buttonSearchMovie.addEventListener("click", searchMovie)
 
 async function searchMovie() {
   const url = BASE_URL
-  
-}
+  await fetch(url).then(requestUrl => {
+    requestUrl.json()
+      .then(dataRequest => {
+        console.log(dataRequest)
+        titleMovie.textContent = dataRequest.original_title
+        descriptionMovies.textContent = dataRequest.overview
+      })
+  }).catch(error => {
+    let containerImg = document.createElement("figure")
+    let imgError = document.createElement("img")
+    imgError.classList.add("img-error")
+    imgError.src = "./assets/poster.svg"
+    containerImg.appendChild(imgError)
+    document.querySelectorAll(".containerMovies figure")[0].appendChild(containerImg)
+    titleMovie.textContent = `Ops, hoje não é dia de assistir filme.
+    Bora codar! 🚀`
+    console.log(error)
+  })
+} 
