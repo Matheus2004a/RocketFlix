2cf1fdf00197d6e0572dea434890c0db5878d98f
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-07-24 15:34:57 -0300

    :goal_net: feat: Valida se foto do filme existe

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { API_KEY, IMG_URL, language } from './api.js'
 
+const body = document.querySelector("body")
 const buttonSearchMovie = document.querySelector(".search-movie")
 buttonSearchMovie.addEventListener("click", searchMovie)
 
@@ -10,32 +11,50 @@ function contentMovie(data) {
   const main = document.querySelector("main")
   const section = document.querySelector(".section-movie")
 
+  function changeHeightScreen() {
+    main.contains(section.firstElementChild) ? body.classList.add("has-content") : body.classList.remove
+  }
+
   const detailsIsUndefined = data.title === undefined && data.overview === undefined && !data.success
 
   if (detailsIsUndefined) {
     section.innerHTML = `<figure>
       <img src=./assets/poster.svg>
         <figcaption>
-          <p id="descriptionMovies">Filme não disponível.
+          <p>Filme não disponível.
           Bora codar! 🚀</p>
         </figcaption>
       </figure>`
     main.prepend(section)
+    changeHeightScreen()
     throw new Error("Filme não disponível")
   }
 
-  const dateFormatted = (`${date.getDate() + 1}/${((date.getMonth() + 1))}/${date.getFullYear()}`); 
+  const dateFormatted = (`${date.getDate() + 1}/${((date.getMonth() + 1))}/${date.getFullYear()}`)
+
+  let statusPhotoMovie = data.poster_path
+
+  if (statusPhotoMovie == null || statusPhotoMovie == undefined) {
+    statusPhotoMovie = `https://img.icons8.com/ios/500/no-image.png`
+    const img = document.querySelector(".section-movie img")
+    img.classList.add("movie-not-found")
+  } else {
+    statusPhotoMovie = `${IMG_URL}/${statusPhotoMovie}`
+  }
+
+  console.log(statusPhotoMovie)
 
   section.innerHTML = `<figure>
-      <img src=${IMG_URL}/${data.poster_path}>
-        <figcaption>
-          <h3 id="titleMovie">${data.title}</h3>
-          <p>Data de lançamento: ${dateFormatted}</p>
-          <p id="descriptionMovies">${data.overview}</p>
-          <p><strong>Gêneros:</strong> ${data.genres.map(item => item.name)}</p>
-        </figcaption>
-      </figure>`
+  <img src=${statusPhotoMovie}>
+  <figcaption>
+  <h3>${data.title}</h3>
+  <p>Data de lançamento: ${dateFormatted}</p>
+  <p>${data.overview}</p>
+  <p><strong>Gêneros:</strong> ${data.genres.map(item => item.name)}</p>
+  </figcaption>
+  </figure>`
   main.prepend(section)
+  changeHeightScreen()
 }
 
 async function searchMovie() {
